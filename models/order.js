9192474d8f434b8bb82f51f25e409e175f44bd5b
William Rave <wrave@gwu.edu>
2020-05-27 11:37:15 -0400

    Wrote Order model

---
+++ models/order.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/*The orders collection will have these fields: 
+`customer` (relationship), `product` (relationship), 
+`qty` (number), `orderDate` (Date, Default to now)*/
+const OrderSchema = new Schema({
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: "Customer"
+    },
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    qty: {
+        type: Number
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const Order = mongoose.model("Order",OrderSchema);
+
+module.exports = Order;
